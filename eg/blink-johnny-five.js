06b6f418bb0aa6b65c1599c571e8d3d71158b81f
Jeff Hoefs <jeff.hoefs@gmail.com>
2015-03-09 10:48:37 -0400

    Change blink pin

--- eg/blink-johnny-five.js
+++ eg/blink-johnny-five.js
@@ -3,6 +3,6 @@ var EtherPort = require("../");
 var board = new five.Board({ port: new EtherPort(3030) });
 
 board.on("ready", function() {
-  var led = new five.Led(13);
+  var led = new five.Led(8);
   led.blink(500);
 });
